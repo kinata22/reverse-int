05d0cf1ed27abe71ad0defc04c4b8bf22b982335
kinata <s@nata.cv.ua>
2022-06-23 14:48:50 +0200

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  const str = Math.abs(n).toString();
+  let i = 0;
+  let result = '';
+  while(i<str.length) {
+    result = `${str[i]}${result}`;
+    i++;
+  }
+  return Number(result);
 }
